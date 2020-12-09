8866731d1f18d344fac0e266adc4b59a50ffe483
Anna <a_form03@uni-muenster.de>
2020-12-09 09:18:31 +0100

    multiple labels in one annotation file

--- trajectory-extraction/object-detection/draw_yolo_coords.py
+++ trajectory-extraction/object-detection/draw_yolo_coords.py
@@ -3,6 +3,7 @@ import csv
 import os
 import argparse
 import cv2
+import numpy as np
 
 # python draw_yolo_coords.py --photos_folder F:\Downloads\database\Allogymnopleuri_#01\Allogymnopleuri_#01_imgs
 
@@ -13,12 +14,17 @@ ap.add_argument("-photos_folder", "--photos_folder", required=True,
 args = vars(ap.parse_args())
 
 
+def get_random_color():
+    color = list(np.random.choice(range(256), size=3))
+    return color
+
 def from_yolo_to_cor(box, shape):
     img_h, img_w, _ = shape
     # x1, y1 = ((x + witdth)/2)*img_width, ((y + height)/2)*img_height
     # x2, y2 = ((x - witdth)/2)*img_width, ((y - height)/2)*img_height
     x1, y1 = int((box[0] + box[2]/2)*img_w), int((box[1] + box[3]/2)*img_h)
     x2, y2 = int((box[0] - box[2]/2)*img_w), int((box[1] - box[3]/2)*img_h)
+    print("bounding box", x1, y1, x2, y2)
     return x1, y1, x2, y2
 
 
@@ -38,24 +44,22 @@ while i < len(txts):
         print("read the image", refImagePath)
         refImage = cv2.imread(refImagePath)
         lines = []
-        with open(refTxtPath) as file:
-            linestrings = [line.rstrip('\n') for line in file]
+        with open(refTxtPath) as f:
+            linestrings = [line.rstrip('\n') for line in f]
             for linestring in linestrings:
                 lines.append(linestring.split())
-            file.close()
 
         if (len(lines) > 0):
-            print(lines)
-        
-        # copy image not to draw over original file
-        copyImage = refImage.copy()
+            # copy image not to draw over original file
+            copyImage = refImage.copy()
 
-        for line in lines:
-            floats = [float(x) for x in line]
-            print(floats)
-            x1, y1, x2, y2 = from_yolo_to_cor(floats[1:], copyImage.shape)
-            cv2.rectangle(copyImage, (x1, y1), (x2, y2), (0,255,0), 3)
-        cv2.imshow("image", copyImage)
-        cv2.waitKey(0)
+            for line in lines:
+                floats = [float(x) for x in line]
+                x1, y1, x2, y2 = from_yolo_to_cor(floats[1:], copyImage.shape)
+
+                random_color = get_random_color()
+                cv2.rectangle(copyImage, (x1, y1), (x2, y2), (int(random_color[0]), int(random_color[1]), int(random_color[2])), 3)
+            cv2.imshow("image", copyImage)
+            cv2.waitKey(0)
 
     i += 1
--- trajectory-extraction/object-detection/parser.py
+++ trajectory-extraction/object-detection/parser.py
@@ -4,15 +4,17 @@ import os
 import argparse
 import cv2
 
-# python parser.py --json Allogymnopleuri_#01_db.grndr --photos_folder F:\Downloads\database\Allogymnopleuri_#01\Allogymnopleuri_#01_imgs --label Beetle
+# python parser.py --json F:\Downloads\database\Allogymnopleuri_#01\Allogymnopleuri_#01_db.grndr --photos_folder F:\Downloads\database\Allogymnopleuri_#01\Allogymnopleuri_#01_imgs --labels Beetle,Ball
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-j", "--json", required=True,
                 help="Path to the json with annotations")
 ap.add_argument("-f", "--photos_folder", required=True,
                 help="Path to the images folder")
-ap.add_argument("-l", "--label", required=True, help="Label of annotations")
+ap.add_argument("-l", "--labels", required=True, help="Label of annotations")
 args = vars(ap.parse_args())
+args["labels"] = args["labels"].split(",")
 
 
 def get_img_shape(path):
@@ -23,10 +25,8 @@ def get_img_shape(path):
         print('error! ', path)
         return (None)
 
-def write_annotation(path, name, position, size):
-    print(path, name, position, size)
-    print(type(position))
-    print(type(size))
+def write_annotation(num, path, name, position, size):
+    print(num, path, name, position, size)
     name_no_ext = os.path.splitext(name)[0]
 
     full_size = get_img_shape(path + '/' + name)
@@ -48,16 +48,17 @@ def write_annotation(path, name, position, size):
         h = h_w[0]*dh
 
         out_path = path + '/annotations/' + name_no_ext + '.txt'
-        outfile = open(out_path, "w")
-        try:
-            line = str(0) + ' ' + str(x) + ' ' + \
-                str(y) + ' ' + str(h) + ' ' + str(w)
-            outfile.write(line)
-            outfile.write("\n")
-            outfile.close()
-        finally:
-            outfile.close()
-
+        if (os.path.isfile(out_path)):
+            with open(out_path, 'a') as fd:
+                line = str(num) + ' ' + str(x) + ' ' + \
+                    str(y) + ' ' + str(h) + ' ' + str(w)
+                fd.write(line)
+        else:
+            with open(out_path, 'w') as fd:
+                line = str(num) + ' ' + str(x) + ' ' + \
+                    str(y) + ' ' + str(h) + ' ' + str(w)
+                fd.write(line)
+                fd.write("\n")
 
 f = open(args['json'])
 data = json.load(f)
@@ -65,38 +66,39 @@ f.close()
 
 print(args)
 
-# check if label we need is there
-for ref in data['Labels']:
-    if (ref['Label']['Name'] == args['label']):
-            # store references for labels and for image names
-        label_references = ref['Label']['ImageBuildPool'][0]['Item']['ImageBuilds']
-        image_references = data['ImageReferences']
+for num, labelName in enumerate(args["labels"]):
+    # check if label we need is there
+    for ref in data['Labels']:
+        if (ref['Label']['Name'] == labelName):
+                # store references for labels and for image names
+            label_references = ref['Label']['ImageBuildPool'][0]['Item']['ImageBuilds']
+            image_references = data['ImageReferences']
 
-        # for each label reference, find coordinates of the top left corner,
-        # the size of the bounding box
-        # and the index of the reference image
-        for x in label_references:
-            index = None
-            position = None
-            size = None
-            name = None
-            # starting with reference for which object is in the image
-            if (len(x['ImageBuild']['Layers']) > 0):
-                if (len(x['ImageBuild']['Layers'][0]['Layer']['DraftItems']) > 0):
-                    index = x['ImageBuild']['ImageReference']
-                    all_properties = x['ImageBuild']['Layers'][0]['Layer']['DraftItems'][0]['DraftItem']['Properties']
+            # for each label reference, find coordinates of the top left corner,
+            # the size of the bounding box
+            # and the index of the reference image
+            for x in label_references:
+                index = None
+                position = None
+                size = None
+                name = None
+                # starting with reference for which object is in the image
+                if (len(x['ImageBuild']['Layers']) > 0):
+                    if (len(x['ImageBuild']['Layers'][0]['Layer']['DraftItems']) > 0):
+                        index = x['ImageBuild']['ImageReference']
+                        all_properties = x['ImageBuild']['Layers'][0]['Layer']['DraftItems'][0]['DraftItem']['Properties']
 
-                    for prop in all_properties:
-                        if (prop['Property']['Name'] == 'Position'):
-                            position = prop['Property']['Value']
-                        if (prop['Property']['Name'] == 'Size'):
-                            size = prop['Property']['Value']
-                    if (index and position and size):
-                        print(index, position, size)
-                        for ref in image_references:
-                            if (ref['ImageReference']['Index'] == index):
-                                path = ref['ImageReference']['File']
-                                base_name = os.path.basename(path)
-                                if (base_name):
-                                    write_annotation(
-                                        args['photos_folder'], base_name, position, size)
+                        for prop in all_properties:
+                            if (prop['Property']['Name'] == 'Position'):
+                                position = prop['Property']['Value']
+                            if (prop['Property']['Name'] == 'Size'):
+                                size = prop['Property']['Value']
+                        if (index and position and size):
+                            print(index, position, size)
+                            for ref in image_references:
+                                if (ref['ImageReference']['Index'] == index):
+                                    path = ref['ImageReference']['File']
+                                    base_name = os.path.basename(path)
+                                    if (base_name):
+                                        write_annotation(
+                                            num, args['photos_folder'], base_name, position, size)

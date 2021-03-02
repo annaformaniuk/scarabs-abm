60b2396fe24434cbefaf743b3baa3bf9a37368fd
Anna <a_form03@uni-muenster.de>
2021-03-02 23:13:46 +0100

    multiprocessing in the distance time group

--- calibration/sensitivity_analysis/distance-time_group.py
+++ calibration/sensitivity_analysis/distance-time_group.py
@@ -6,22 +6,24 @@ from multiprocessing import Pool
 
 import pyNetLogo
 
+
 def initializer(modelfile):
     '''initialize a subprocess
-    
+
     Parameters
     ----------
     modelfile : str
-    
+
     '''
-    
+
     # we need to set the instantiated netlogo
     # link as a global so run_simulation can
     # use it
     global netlogo
-    
+
     netlogo = pyNetLogo.NetLogoLink(gui=False)
     netlogo.load_model(modelfile)
+
 
 def run_simulation(experiment):
     '''run a netlogo model
@@ -35,6 +37,8 @@ def run_simulation(experiment):
     print('Experiment', experiment)
     netlogo.command('setup')
 
+    save_value = next(iter(experiment.values()))
+
     # Set the input parameters
     for key, value in experiment.items():
         if key == 'random-seed':
@@ -47,7 +51,8 @@ def run_simulation(experiment):
 
     netlogo.command('setup')
     # Run for 1000 ticks and return the number of beetles and their mean speeds
-    counts = netlogo.repeat_report(['total-distances-walked', 'total-durations-walked'], 6000)
+    counts = netlogo.repeat_report(
+        ['total-distances-walked', 'total-durations-walked'], 6000)
 
     print("Done")
 
@@ -60,8 +65,8 @@ def run_simulation(experiment):
     mean_time = last_state_time.mean()
     std_time = np.std(last_state_time)
 
-    print(mean_dist, std_dist, mean_time, std_time)
-    return mean_dist, std_dist, mean_time, std_time
+    print(save_value, mean_dist, std_dist, mean_time, std_time)
+    return save_value, mean_dist, std_dist, mean_time, std_time
 
 
 if __name__ == '__main__':
@@ -76,32 +81,53 @@ if __name__ == '__main__':
 
     # add patch roughness!
     problem = {
-        'names': ['distance-threshold-impact', 'last-seen-threshold-impact', 'seen-radius-impact', 'heading-memory-impact'],
+        'names': ['distance-threshold-impact', 'last-seen-threshold-impact', 'seen-radius-impact', 'heading-memory-impact', 'patch-roughness-impact'],
         'bounds': bounds,
-        'mean_dist': [[], [], [], []],
-        'std_dist': [[], [], [], []],
-        'mean_time': [[], [], [], []],
-        'std_time': [[], [], [], []]
+        'mean_dist': [[], [], [], [], []],
+        'std_dist': [[], [], [], [], []],
+        'mean_time': [[], [], [], [], []],
+        'std_time': [[], [], [], [], []]
     }
 
-    
-        # with Pool(4, initializer=initializer, initargs=(modelfile,)) as executor:
-        # results = []
-        # for entry in executor.map(run_simulation, experiments.to_dict('records')):
-        #     results.append(entry)
-        # results = pd.DataFrame(results)
-
+    # with Pool(4, initializer=initializer, initargs=(modelfile,)) as executor:
+    # results = []
+    # for entry in executor.map(run_simulation, experiments.to_dict('records')):
+    #     results.append(entry)
+    # results = pd.DataFrame(results)
 
     for i in range(len(problem['names'])):
-        for value in problem['bounds']:
-            netlogo.load_model(modelfile)
-            experiment = {problem['names'][i]: value}
-            mean_dist, std_dist, mean_time, std_time = run_simulation(experiment)
+        # for value in problem['bounds']:
+        #     netlogo.load_model(modelfile)
+        #     experiment = {problem['names'][i]: value}
+        #     mean_dist, std_dist, mean_time, std_time = run_simulation(experiment)
 
-            problem['mean_dist'][i].append(mean_dist)
-            problem['std_dist'][i].append(std_dist)
-            problem['mean_time'][i].append(mean_time)
-            problem['std_time'][i].append(std_time)
+        #     problem['mean_dist'][i].append(mean_dist)
+        #     problem['std_dist'][i].append(std_dist)
+        #     problem['mean_time'][i].append(mean_time)
+        #     problem['std_time'][i].append(std_time)
+        with Pool(4, initializer=initializer, initargs=(modelfile,)) as executor:
+            experiments = pd.DataFrame(problem['bounds'],
+                                       columns=[problem['names'][i]])
+            # placeholders
+            result_mean_dist = np.empty_like(problem['bounds'])
+            result_std_dist = np.empty_like(problem['bounds'])
+            result_mean_time = np.empty_like(problem['bounds'])
+            result_std_time = np.empty_like(problem['bounds'])
+            for input_value, mean_dist, std_dist, mean_time, std_time in executor.map(run_simulation, experiments.to_dict('records')):
+                print("receiving", input_value, mean_dist,
+                      std_dist, mean_time, std_time)
+                current_index = np.where(
+                    problem['bounds'] == input_value)[0][0]
+                print(current_index)
+                result_mean_dist[current_index] = mean_dist
+                result_std_dist[current_index] = std_dist
+                result_mean_time[current_index] = mean_time
+                result_std_time[current_index] = std_time
+
+            problem['mean_dist'][i] = result_mean_dist
+            problem['std_dist'][i] = result_std_dist
+            problem['mean_time'][i] = result_mean_time
+            problem['std_time'][i] = result_std_time
 
     fig, (ax1, ax2) = plt.subplots(2)
 
@@ -111,7 +137,7 @@ if __name__ == '__main__':
     # ax.set_ylim([0,20])
     for i in range(len(problem['names'])):
         ax1.errorbar(problem['bounds'], problem['mean_dist'][i],
-                    yerr=problem['std_dist'][i], color=colors[i], label=problem['names'][i])
+                     yerr=problem['std_dist'][i], color=colors[i], label=problem['names'][i])
     ax1.legend()
 
     ax2.set_xlabel('Multiplication factor', fontsize=16)
@@ -120,9 +146,7 @@ if __name__ == '__main__':
     # ax.set_ylim([0,20])
     for i in range(len(problem['names'])):
         ax2.errorbar(problem['bounds'], problem['mean_time'][i],
-                    yerr=problem['std_time'][i], color=colors[i], label=problem['names'][i])
+                     yerr=problem['std_time'][i], color=colors[i], label=problem['names'][i])
     ax2.legend()
 
     plt.show()
-
-

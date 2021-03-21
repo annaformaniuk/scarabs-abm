82c7816ecbb35bf4895328902c7a35fc2417a7da
Anna <a_form03@uni-muenster.de>
2021-03-21 21:20:05 +0100

    some error prevention

--- trajectory-extraction/video_to_displacement_vectors.py
+++ trajectory-extraction/video_to_displacement_vectors.py
@@ -15,7 +15,9 @@ import matplotlib.pyplot as plt
 from imutils.video import count_frames
 # from geojson import Point, Feature, FeatureCollection, dump, lat, lon
 import json
-# python video_to_displacement_vectors.py --video_path "F:\Dokumente\Uni_Msc\Thesis\videos\Allogymnopleuri_Rolling from dung pat_201611\resized\cut\Lamarcki_#01_Rolling from dung pat_20161114_cut_720_supershort.mp4" --ball_size 3
+# python video_to_displacement_vectors.py --video_path "F:\Dokumente\Uni_Msc\Thesis\videos\Cut_trajectories\not_processed\Ambiguus_#14_Rolling from dung pat_20161117_cut.mp4" --ball_size 2
+
+# python video_to_displacement_vectors.py --video_path "F:\Dokumente\Uni_Msc\Thesis\videos\Allogymnopleuri_Rolling from dung pat_201611\resized\cut\Lamarcki_#01_Rolling from dung pat_20161114_cut_720_supershort.mp4" --ball_size 2
 
 
 # construct the argument parser and parse the arguments
@@ -26,6 +28,7 @@ ap.add_argument("-ball_size", "--ball_size", required=True,
                 help="Path to the video")
 args = vars(ap.parse_args())
 
+FIRST_FRAME = 93
 
 def mask_out_objects(frame, objects):
     height, width, depth = frame.shape
@@ -83,6 +86,8 @@ def save_geojson(points, vectors_array, name, ball_diagonal, ball_size, i):
 
     points_json["points"].pop(0)
 
+    print('ready to save')
+
     print(points_json)
 
     filename = "trajectory_"+ name + ".json"
@@ -106,13 +111,18 @@ def reproduce_trajectory(displacement_vectors, diagonals, name, ball_size, i):
     # scaling the displacement vectors to the size of the ball because
     # it's the only constant object in the frames
     scale = reference_diagonal/diagonals_array
+
+    print("what is going oooonnn")
+    print(vectors_array)
+    test = scale[:, np.newaxis]
+    print(test)
     diagonals_scaled_floats = np.multiply(vectors_array, scale[:, np.newaxis])
     diagonals_scaled = diagonals_scaled_floats.astype(int)
 
     starting_point = diagonals_scaled.sum(axis=0)
     starting_point_array = np.array(starting_point)
     # to start in the middle
-    width = abs(starting_point[0]) + abs(starting_point[0])
+    width = abs(starting_point[0]) + abs(starting_point[0])*10
     height = abs(starting_point[1]) + abs(starting_point[1])
 
     trajectory = starting_point_array + np.cumsum(diagonals_scaled, axis=0)
@@ -139,7 +149,7 @@ def reproduce_trajectory(displacement_vectors, diagonals, name, ball_size, i):
     # cv2.imshow('black_img', black_img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    cv2.imwrite('trajectory_reconstruction_SHORT.png', black_img)
+    cv2.imwrite(str(name) + '_trajectory_reconstruction.png', black_img)
 
     print(vectors_array)
 
@@ -149,6 +159,8 @@ def reproduce_trajectory(displacement_vectors, diagonals, name, ball_size, i):
 
 
 if (os.path.isfile(args["video_path"])):
+# def process_video(args):
+    print(args)
     cap = cv2.VideoCapture(args["video_path"])
     yolo = Yolo_detector()
     contours = Contours_detector()
@@ -169,7 +181,7 @@ if (os.path.isfile(args["video_path"])):
         i = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
         if ret:
-            if (i == 1):
+            if (i == FIRST_FRAME):
                 imReference = frame.copy()
                 # that's what the previously matched frames will become, so that they are not used as reference
                 height, width, depth = frame.shape
@@ -199,10 +211,15 @@ if (os.path.isfile(args["video_path"])):
                     (x for x in objects if x["label"] == "Ball"), None)
                 if(ball_bounds != None):
                     first_ball_diagonal = get_diagonal(ball_bounds["box"])
+                # else:
+                #     first_ball_diagonal = None
                 
                 frame_counts.append(i)
 
-            if (i > 1 and i < 6000 and i % 31 == 0):
+                print('first ball!', first_ball_diagonal)
+
+            if (i > FIRST_FRAME and i % 31 == 0):
+                print('processing frame', i)
                 height, width, depth = frame.shape
                 objects = yolo.detect_objects(frame)
                 # masking out detected objects so that they won't be used as keypoins
@@ -224,24 +241,29 @@ if (os.path.isfile(args["video_path"])):
                 if(beetle_bounds != None):
                     second_coords = get_centroid(beetle_bounds["box"])
 
-                ball_bounds = next(
-                    (x for x in objects if x["label"] == "Ball"), None)
-                ball_diagonal = None
-                if(ball_bounds != None):
-                    ball_diagonal = get_diagonal(ball_bounds["box"])
-                else:
-                    ball_diagonal = first_ball_diagonal
-                ball_diagonals.append(ball_diagonal)
-                frame_counts.append(i)
+                    ball_bounds = next(
+                        (x for x in objects if x["label"] == "Ball"), None)
+                    ball_diagonal = None
+                    if(ball_bounds != None):
+                        ball_diagonal = get_diagonal(ball_bounds["box"])
+                        ball_diagonals.append(ball_diagonal)
+                        frame_counts.append(i)
+                    else:
+                        ball_diagonal = first_ball_diagonal
+                        ball_diagonals.append(ball_diagonal)
+                        frame_counts.append(i)
+                    
 
-                # finally stitching the images together and replacing variables
-                matched_image, matched_coords = stitching_alt.match_pairwise(
-                    frame, imReference, foreground_mask, background_mask, landscapeReference, landscapeFront, second_coords)
+                    # finally stitching the images together and replacing variables
+                    matched_image, matched_coords = stitching_alt.match_pairwise(
+                        frame, imReference, foreground_mask, background_mask, landscapeReference, landscapeFront, second_coords)
 
-                # calculate the displacement vector between first_coords and matched_coords
-                displacement_vector = get_displacement_vector(
-                    first_coords, matched_coords)
-                displacement_vectors.append(displacement_vector)
+                    # calculate the displacement vector between first_coords and matched_coords
+                    displacement_vector = get_displacement_vector(
+                        first_coords, matched_coords)
+                    displacement_vectors.append(displacement_vector)
+
+                    first_coords = second_coords
 
                 # plt.gca().invert_yaxis()
                 # print(first_coords[0], first_coords[1],
@@ -254,15 +276,22 @@ if (os.path.isfile(args["video_path"])):
                 imReference = frame
                 landscapeReference = landscapeFront
                 background_mask = foreground_mask
-                first_coords = second_coords
+
+                # print(ball_diagonals)
+
+                ball_diagonals = list(
+                    map(lambda x: int(np.average([y for y in ball_diagonals if y != None])) if x == None else x, ball_diagonals))
+
+                # print(ball_diagonals)
 
             # if (i == total_frames_count - 1):
 
             if (i > total_frames_count - 10):
                 print(ball_diagonals)
                 print(frame_counts)
-                # ball_diagonals = np.array(
-                #     map(lambda x: first_ball_diagonal if x == None else x, ball_diagonals))
+                ball_diagonals = list(
+                    map(lambda x: int(np.average([y for y in ball_diagonals if y != None])) if x == None else x, ball_diagonals))
+                print(ball_diagonals)
                 reproduce_trajectory(displacement_vectors, ball_diagonals, filename, args["ball_size"], frame_counts)
                 break
 
